bcb1452ebda9872630132c162fd5d7ad661e904b
Vicu Gabriel-Alexandru <gabi.vicu@gmail.com>
2023-02-21 16:38:07 +0200

    Created Author controller

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return the list of authors
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Create a new author
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+    
+    /**
+     * Show details of an author
+     * @return Illuminate\Http\Response
+     */
+    public function show($author)
+    {
+
+    }
+
+        /**
+     * Updates an existing author
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $author)
+    {
+
+    }
+
+        /**
+     * Delete an author
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($author)
+    {
+
+    }
+}
